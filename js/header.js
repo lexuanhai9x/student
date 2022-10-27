59702d0cd0502719a14365480b01dd0a413b57f7
lexuanhai9x <110213939+lexuanhai9x@users.noreply.github.com>
2022-10-27 14:06:19 +0700

    u

--- js/header.js
+++ js/header.js
@@ -1,4 +1,20 @@
 var nutMenu = document.querySelector(".menu-button");
+var menuNav = document.querySelector('ul.nav');
+var tinhClick = 0;
 nutMenu.addEventListener("click",function(){
-    nutMenu.classList.toggle("active");
+    if(tinhClick == 0){
+        tinhClick++;
+        menuNav.classList.remove('active2');
+        nutMenu.classList.add("active");
+        menuNav.classList.add('active');
+    }
+    else if(tinhClick == 1){
+        tinhClick=0;
+        nutMenu.classList.remove("active");
+        menuNav.classList.remove('active');
+        menuNav.classList.add('active2');
+    }
+})
+nutMenu.addEventListener('mouseover',function(){
+    menuNav.classList.remove('active2');
 })
